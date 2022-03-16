a0fb8d8de32f44b2e1fa7b6903b28aa0c95ef33b
Arati121 <avbhosale@mitaoe.ac.in>
2022-03-16 16:01:56 +0530

    Generic class,method

---
+++ collection/Genric_Swap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace collection
+{
+    class Genric_Swap
+    {
+       
+            static void swap<T>(ref T a,ref T b)
+            {
+                T temp;
+                temp = a;
+                a = b;
+                b = temp;
+            }
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            swap<int>(ref a, ref b);
+            Console.WriteLine(a+" "+b);
+
+        }
+    }
+}
